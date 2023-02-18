60c2919c45e392d87ec023963425aa9b1d64dc65
Mateusz Kalenik <UL0253571@edu.uni.lodz.pl>
2023-02-18 13:34:15 +0100

    InputController

---
+++ src/main/java/com/sda/controller/InputController.java
@@ -0,0 +1,49 @@
+package com.sda.controller;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Scanner;
+@Slf4j
+public class InputController {
+
+    private final Scanner scanner;
+
+    public InputController() {
+        scanner = new Scanner(System.in);
+    }
+
+    public String getString(String text) {
+        System.out.print(text);
+        return scanner.nextLine();
+    }
+
+    public String getUsername() {
+        return getString("Enter username: ");
+    }
+
+    public String getName() {
+        return getString("Enter name: ");
+    }
+
+    public String getSurname() {
+        return getString("Enter surname: ");
+    }
+
+    public int getAge() {
+        String ageStr = getString("Enter age: ");
+        try {
+            return Integer.parseInt(ageStr);
+        } catch (NumberFormatException e) {
+            log.error("Invalid age input! Defaulting to 0.");
+            return 0;
+        }
+    }
+
+    public String getEmail() {
+        return getString("Enter email: ");
+    }
+
+    public String getPassword() {
+        return getString("Enter password: ");
+    }
+}
